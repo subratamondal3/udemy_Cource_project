605413c6c1e1b0bd2726b548a263bbca2fe2928c
subrata <jobssubrata2@gmail.com>
2025-12-28 00:26:02 +0530

    project upload

---
+++ MyProject/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+namespace MyProject.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [Route("api/[controller]/[action]")]
+        [Route("/")]
+        public ContentResult Index()
+        {
+            return Content("<h1>Welcome </h1> <h4>Hello from index</h4>",
+                "text/html");
+        }
+        [Route("api/[controller]/[action]")]
+        public JsonResult Person()
+        {
+            Person person = new Person()
+            {
+                Id = 1,
+                Name = "John",
+                Age = 25
+            };
+            // return new JsonResult(person);
+            return Json(person);
+        }
+        public string Contact()
+        {
+            return "Hello from Contact";
+        }
+
+        [Route("download")]
+        public VirtualFileResult Download()
+        {
+            return File("/mong.pdf", "application/pdf");
+        }
+        [Route("download1")]
+        public PhysicalFileResult Download1()
+        {
+            return PhysicalFile(@"C:\Users\subrata mondal\Downloads\asp.net.interview.pdf", "application/pdf");
+        }
+        [Route("download2")]
+        public FileContentResult Download2()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(@"C:\Users\subrata mondal\Downloads\asp.net.interview.pdf");
+            return File(fileBytes, "application/pdf");
+        }
+
+    }
+}
---
+++ MyProject/MyCustomMiddlewareClass.cs
@@ -0,0 +1,12 @@
+﻿namespace MyProject
+{
+    public class MyCustomMiddlewareClass : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello from MyCustomMiddlewareClass --Starts");
+            await next(context);
+            await context.Response.WriteAsync("Hello from MyCustomMiddlewareClass --Ends");
+        }
+    }
+}
